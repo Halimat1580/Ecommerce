2dadaad16bc4252d5fe82658ebfe6b3e97c34914
Halimat1580 <halimausman43@yahoo.com>
2023-11-06 11:36:33 +0100

    https://github.com/Halimat1580/mywork.git01

---
+++ src/Layouts/NavBar.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Marque from './Marque'
+import '../Styles/NavBar.css'
+import { BsCart4 } from 'react-icons/bs';
+
+// images
+
+const NavBar = ({cart}) => {
+  return (
+    <div className='sticky-top'>
+        <Marque/>
+        <div className="navbar-content">
+        <header className='container d-flex justify-content-between align-items-center'>
+            <ul>
+                <li className="list-unstyled">
+                    <Link className='text-decoration-none' to='/'>
+                    <h2 className='fst-italic text-white'>Halimat's Store</h2>
+                    </Link>
+                </li>
+            </ul>
+            <div className="w-50  d-none d-lg-block">
+          <form className="">
+            <input
+              type='search'
+              placeholder='search products, brands and categories...'
+              className="w-100 form-control "
+            />
+          </form>
+          </div>
+            {/* nav link */}
+            <nav>
+                <ul className=' gap-4 d-flex list-unstyled'>
+                    <li>
+                        <Link className="text-decoration-none text-light fs-4" to='/'>
+                        Account
+                        </Link>
+                    </li>
+
+                    <li>
+                    <Link className="text-decoration-none text-light fs-4" to='/'> 
+                    Help
+                    </Link>
+                    </li>
+
+                    <li>
+                    <Link className="text-decoration-none text-light fs-4" to='/Cart'>
+                      <BsCart4/>
+                      ({cart.length})
+                    </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        </div>
+       
+        </div>
+  );
+};
+
+export default NavBar
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+
+import React, { useEffect } from 'react'
+import Hero from '../Component/Hero'
+
+const Home = ({cart,handleAddToCart, setCart}) => {
+    useEffect(()=>{
+        document.title = 'Home | Page'
+
+    })
+        
+    
+  return (
+    <div className='container'>
+        <Hero cart={cart} handleAddToCart={handleAddToCart} setCart={setCart}/>
+    </div>
+  )
+}
+
+export default Home
